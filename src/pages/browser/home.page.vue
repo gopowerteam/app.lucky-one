0106580dad4a04a4d8ad23d9c7e6ef95668e83a8
黄瑞 <hr837@163.com>
2019-09-23 08:59:48 +0800

    添加首页 (#10)

--- src/pages/browser/home.page.vue
+++ src/pages/browser/home.page.vue
@@ -1,6 +1,21 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="@/assets/images/logo.png" />
+    <div class="home-container">
+      <div class="create-room">
+        <q-btn>创建房间</q-btn>
+      </div>
+      <div class="rooms">
+        <room-card></room-card>
+        <room-card roomName="房间2"></room-card>
+        <room-card roomName="房间3"></room-card>
+        <room-card roomName="房间4"></room-card>
+        <room-card roomName="房间5"></room-card>
+      </div>
+      <div class="nav-buttoms">
+        <q-btn>←</q-btn>
+        <q-btn>→</q-btn>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -10,8 +25,13 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import { RoomService } from '../../services/room.service'
+import RoomCard from '@/components/home/room-card.vue'
 
-@Component
+@Component({
+  components:{
+    RoomCard
+  }
+})
 export default class HomePage extends Vue {
   public roomService = new RoomService()
 
@@ -34,3 +54,28 @@ export default class HomePage extends Vue {
   }
 }
 </script>
+
+<style lang="less" scoped>
+.home-page {
+  .home-container {
+    width: 70%;
+    height: 500px;
+    box-shadow: 0px 0px 25px 5px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+  }
+  .create-room,
+  .nav-buttoms {
+    text-align: right;
+    margin: 30px;
+  }
+  .rooms {
+    padding-left: 100px;
+    white-space: nowrap;
+    overflow: hidden;
+    .room-card {
+      margin: 15px 50px 15px 0;
+      display: inline-block;
+    }
+  }
+}
+</style>
