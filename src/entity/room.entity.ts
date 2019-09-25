bd3d800acc9115723bb6b4ac561a5a3016f6863d
zhuchentong <zhuchentong@zhuchentongs-Mac-mini.local>
2019-09-25 23:37:17 +0800

    wip: 添加奖项服务

--- src/entity/room.entity.ts
+++ src/entity/room.entity.ts
@@ -2,12 +2,14 @@ import AV from 'leancloud-storage'
 import { Entity } from './'
 import { RealtimeUtil } from '@/shared/utils/realtime.util'
 import { ConversationBase } from 'leancloud-realtime'
+import { RoomService } from '@/services/room.service'
+import { AwardService } from '@/services/award.service'
 
 export class RoomEntity extends Entity {
   /**
    * 会话实例
    */
-  private conversation
+  private conversation?:ConversationBase
 
   public getEnable() {
     return this.get('enable')
@@ -27,6 +29,9 @@ export class RoomEntity extends Entity {
     }
   }
 
+  /**
+   * 
+   */
   public async getConversation(): Promise<void | ConversationBase> {
     const realtimeUtil = new RealtimeUtil()
 
@@ -35,9 +40,21 @@ export class RoomEntity extends Entity {
     }
 
     if (this.attributes.enable && this.valid) {
-      this.conversation = await realtimeUtil.getConversation(this.attributes.conversation)
+      const conversation = await realtimeUtil.getConversation(this.attributes.conversation)
+      this.conversation = conversation as ConversationBase
     } else {
       throw new Error('当前会话未启用')
     }
+  }
+
+  /**
+   * 创建奖
+   *  
+   *         
+   * @param params 
+   */
+  public createAward(params){
+    const awardService = new AwardService()
+    return awardService.create(this.object,params)
   }
 }
--- src/services/award.service.ts
+++ src/services/award.service.ts
@@ -1,3 +1,67 @@
 import AV from 'leancloud-storage'
+import { Entity } from '@/entity'
+import { AwardEntity } from '@/entity/award.entity'
+import { RoomEntity } from '@/entity/room.entity'
 
-export class AwardService {}
+const Award = AV.Object.extend('Award')
+
+export class AwardService {
+  /**
+   * 获取奖项
+   * @param id 
+   */
+  public async getAward(id) {
+    const query = new AV.Query('award')
+    const award = await query.equalTo('id', id).first()
+    return Entity.from(award, AwardEntity)
+  }
+
+  /**
+   * 设置奖项结果
+   * @param award 
+   * @param count 
+   * @param exclude 
+   */
+  public async setResult(award, count = 1, exclude) {
+    const roomEntity = Entity.from(award.get('room'), RoomEntity)
+    const conversation = await roomEntity.getConversation()
+
+    if (!conversation) {
+      throw Error('房间未启用')
+    }
+
+    // 抽取中奖用户
+    const result = conversation.members
+      .map(x => ({ id: x, seed: Math.random() }))
+      .sort((x, y) => x.seed - y.seed)
+      .slice(0, count)
+    award.set('result', result)
+    award.set('finish', true)
+
+    return award.save()
+  }
+
+  /**
+   * 创建奖项
+   * @param room 
+   * @param param1 
+   */
+  public async create(room, { name, count }) {
+    const award = new Award()
+    const id = Math.random()
+      .toString(36)
+      .substr(2)
+    // 创建奖项
+    const object = await award.save({
+      id,
+      name,
+      count,
+      room,
+      finish: false
+    })
+    // 关联房间
+    room.set('awards', [...(room.awards || []), object])
+    await room.save()
+    return object
+  }
+}
