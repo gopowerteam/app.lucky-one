95e74a33c6d77533df7dcd2cea7bcc1d5161a624
zhuchentong <2037630@163.com>
2019-09-30 16:22:04 +0800

    fix: 测试自动部署

--- src/core/launch/leancloud.ts
+++ src/core/launch/leancloud.ts
@@ -1,15 +1,18 @@
 import AV from 'leancloud-storage'
 import { Realtime } from 'leancloud-realtime'
 
+const appId = 'PsheKGj7iDalocuWVreooVFw-gzGzoHsz'
+const appKey = 'NR9giTdN1QNRw8Wp2L7Ffeq7'
+
 export default async function() {
   // leancloud-storage
   AV.init({
-    appId: process.env.VUE_APP_LEANCLOUD_APPID as string,
-    appKey: process.env.VUE_APP_LEANCLOUD_APPKEY as string
+    appId,
+    appKey
   })
   // leancloud-realtime
   globalThis['$realtime'] = new Realtime({
-    appId: process.env.VUE_APP_LEANCLOUD_APPID as string,
-    appKey: process.env.VUE_APP_LEANCLOUD_APPKEY as string
+    appId,
+    appKey
   })
 }
